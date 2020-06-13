7c530c98ff74c61494fdc22dbe75b2c15451de0a
Andre Macedo <andre.macedo@sikur.com>
2020-06-13 20:56:58 -0300

    commit inicial

---
+++ CCRHakcton/CCRHakcton/Extenders/Controls/CustomEntry.cs
@@ -0,0 +1,44 @@
+﻿using Xamarin.Forms;
+
+namespace Core
+{
+    public class CustomEntry : Entry
+    {
+        public static readonly BindableProperty HasErrorProperty =
+            BindableProperty.Create(nameof(HasError),
+                typeof(bool),
+                typeof(CustomEntry));
+
+        public bool HasError
+        {
+            get => (bool)GetValue(HasErrorProperty);
+            set => SetValue(HasErrorProperty, value);
+        }
+
+        public static readonly BindableProperty PaddingProperty =
+            BindableProperty.Create(nameof(Padding),
+                typeof(Thickness),
+                typeof(CustomEntry));
+
+        public Thickness Padding
+        {
+            get => (Thickness)GetValue(PaddingProperty);
+            set => SetValue(PaddingProperty, value);
+        }
+
+    }
+
+    public class CustomEditor : Editor
+    {
+        public static readonly BindableProperty PaddingProperty =
+            BindableProperty.Create(nameof(Padding),
+                typeof(Thickness),
+                typeof(CustomEntry));
+
+        public Thickness Padding
+        {
+            get => (Thickness)GetValue(PaddingProperty);
+            set => SetValue(PaddingProperty, value);
+        }
+    }
+}
---
+++ CCRHakcton/CCRHakcton/Features/Login/LoginViewModel.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Core
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        #region Properties
+        string _user;
+        public string User
+        {
+            get => _user;
+            set => SetProperty(ref _user, value);
+        }
+
+        string _password;
+        public string Password
+        {
+            get => _password;
+            set => SetProperty(ref _password, value);
+        }
+
+        string _error = string.Empty;
+        public string Error
+        {
+            get => _error;
+            set => SetProperty(ref _error, value);
+        }
+        #endregion
+
+        #region Services
+        readonly ILoginService _loginService;
+        readonly IDialogService _dialogService;
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand { get; }
+        public ICommand NeedHelpCommand { get; }
+        public ICommand ChangeUserCommand { get; set; }
+        #endregion
+
+        #region Init
+        public LoginViewModel(INavigationService navigationService,
+                              ILoginService loginService,
+                              IDialogService dialogService) : base(navigationService)
+        {
+            _loginService = loginService;
+            _dialogService = dialogService;
+
+            LoginCommand = new Command(async () => await ExecuteLoginCommandAsync());
+            NeedHelpCommand = new Command(async () => await ExecuteNeedHelpCommandAsync());
+            ChangeUserCommand = new Command((user) => ExecuteChangeUserCommand(user));
+        }
+
+        #endregion
+
+        #region ExecuteCommand
+
+        void ExecuteChangeUserCommand(object user)
+        {
+            if(user is string usr)
+            {
+                switch (usr)
+                {
+                    case "Peter":
+                        User = "peter.novassat@sikur.dev";
+                        break;
+                    case "Andre":
+                        User = "andre.macedo@sikur.dev";
+                        break;
+                    case "Tiago":
+                        User = "sikurdev+1@sikur.dev";
+                        break;
+                    case "Bruno":
+                        User = "bruno.silva@sikur.dev";
+                        break;
+                    default:
+                        break;
+                }
+                Password = "Sikur@123";
+            }
+        }
+        async Task ExecuteLoginCommandAsync()
+        {
+            Error = string.Empty;
+
+            var loginResult = await _loginService.LoginAsync(User, Password).Handle(this);
+
+            Password = string.Empty;
+
+            if (!loginResult.Success)
+            {
+                Error = loginResult.Message;
+                return;
+            }
+
+            if (!loginResult.Data.Complete)
+            {
+                if (loginResult.Data.Message == Resource.NeedsEmailConfirmation)
+                {
+                    var resend = await _dialogService.DisplayAsync(string.Empty, Resource.NeedsEmailConfirmation, Resource.SendEmailAgain, Resource.Ok);
+                    if (resend)
+                    {
+                        var resendResult = await _loginService.ResendEmailConfirmationAsync(User).Handle(this);
+                        if (!resendResult.Success)
+                        {
+                            Error = Resource.CantSendEmail;
+                            return;
+                        }
+                    }
+
+                    return;
+                }
+
+                Error = loginResult.Data.Message;
+
+                return;
+            }
+            Preferences.Set(Constants.FirstLauch, true);
+            await _navigationService.NavigateAndClearBackStackAsync<PinAccessViewModel>(PinAccessType.Create);
+        }
+        
+        async Task ExecuteNeedHelpCommandAsync()
+            => await _navigationService.NavigateToAsync<NeedHelpViewModel>();
+        #endregion
+    }
+}
