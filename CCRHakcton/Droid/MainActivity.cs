18acbe25af87697606bd6a24f2ada66c9fc648ac
Andre Macedo <andre.macedo@sikur.com>
2020-06-14 03:54:27 -0300

    correção da splash

--- CCRHakcton/Droid/MainActivity.cs
+++ CCRHakcton/Droid/MainActivity.cs
@@ -10,7 +10,7 @@ using Core;
 
 namespace Droid
 {
-    [Activity(Label = "CCRHakcton", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "CCRHakcton", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
